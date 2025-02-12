aaeed644cbdc01a8e21bfcf67cba51e451d2b586
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-02-12 20:58:24 +0530

    Added OYC Defender

--- resources/[qb]/qb-core/shared/vehicles.lua
+++ resources/[qb]/qb-core/shared/vehicles.lua
@@ -767,6 +767,7 @@ local Vehicles = {
     --- Custom Vehicles
     { model = 'jdbullet2.0',         name = 'Royal Enfield Bullet 350',  brand = 'Royal Enfield',   price = 100000,  category = 'motorcycles',    type = 'bike',       shop = 'pdm' },
     { model = 'defender',         name = 'Land Rover Defender',  brand = 'Land Rover',   price = 100000,  category = 'offroad',    type = 'automobile',       shop = 'pdm' },
+    { model = 'oycdefender',         name = 'Land Rover Defender 2',  brand = 'Land Rover',   price = 100000,  category = 'offroad',    type = 'automobile',       shop = 'pdm' },
 
 
 }
