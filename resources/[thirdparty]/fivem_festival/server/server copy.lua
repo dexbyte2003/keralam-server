7b21dcc2bd56a8d43777001e5cbcc5d4009a9734
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-03-04 19:47:38 +0530

    fixed festival script server.lua bug and backuped old server.lua

--- resources/[thirdparty]/fivem_festival/server/server copy.lua
+++ resources/[thirdparty]/fivem_festival/server/server copy.lua
@@ -14,18 +14,24 @@ function isValidStation(station)
     return false
 end
 
--- Triggered when "qb-djs:getSpeakers" event is called
+-- Triggered when "qb-djs:getEmitters" event is called
 RegisterNetEvent("qb-djs:getSpeakers")
-AddEventHandler("qb-djs:getSpeakers", function(type)
+AddEventHandler("qb-djs:getSpeakers", function()
     local source = source
     if source then
+<<<<<<< HEAD
         if type == "new" then
             -- Sends custom emitter data to the source only
-            TriggerClientEvent("spawnCustomEmittersClient", source, Config.customEmittersData)
+            TriggerClientEvent("spawnCustomEmittersClient", source,
+                Config.customEmittersData)
         else
             -- Sends custom emitter data to all clients
-            TriggerClientEvent("spawnCustomEmittersClient", -1, Config.customEmittersData)
+            TriggerClientEvent("spawnCustomEmittersClient", -1,
+                Config.customEmittersData)
         end
+=======
+
+>>>>>>> 72e210b047d2e282a7a0c0cab7ea24ad4e509820
     else
         print("Invalid player ID: " .. tostring(source))
     end
@@ -49,7 +55,8 @@ AddEventHandler('resumeMusic', function()
     local source = source
     if source then
         -- Sends the new radio station to the client
-        TriggerClientEvent("resumeMusicOnEmitters", -1, Config.customEmittersData)
+        TriggerClientEvent("resumeMusicOnEmitters", -1,
+            Config.customEmittersData)
     else
         print("Invalid player ID: " .. tostring(source))
     end
@@ -71,13 +78,16 @@ AddEventHandler('changeRadioStation', function(newStation)
         print("Invalid player ID: " .. tostring(source))
     end
 end)
+<<<<<<< HEAD
 
 -- Command to spawn the custom emitters at their specified coordinates (/boom)
 RegisterCommand("startfestival", function(source, args)
+    local player = source
     if source then
-        print("Spawning custom emitters for player " .. source)
+        print("Spawning custom emitters for player " .. player)
         -- Triggers the client event to spawn custom emitters for the player
-        TriggerClientEvent("spawnCustomEmittersClient", -1, Config.customEmittersData)
+        TriggerClientEvent("spawnCustomEmittersClient", -1,
+            Config.customEmittersData)
     else
         print("Invalid player ID: " .. tostring(source))
     end
@@ -85,8 +95,10 @@ end, true)
 
 -- Command to pause music
 RegisterCommand("pausemusic", function(source, args)
+    local player = source
     if source then
-        TriggerClientEvent("pauseMusicOnEmitters", -1, Config.customEmittersData)
+        TriggerClientEvent("pauseMusicOnEmitters", -1,
+            Config.customEmittersData)
     else
         print("Invalid player ID: " .. tostring(source))
     end
@@ -94,8 +106,10 @@ end, true)
 
 -- Command to resume music
 RegisterCommand("resumemusic", function(source, args)
+    local player = source
     if source then
-        TriggerClientEvent("resumeMusicOnEmitters", -1, Config.customEmittersData)
+        TriggerClientEvent("resumeMusicOnEmitters", -1,
+            Config.customEmittersData)
     else
         print("Invalid player ID: " .. tostring(source))
     end
@@ -103,12 +117,14 @@ end, true)
 
 -- Command to change the radio station for all emitters (/changeradio <radiostation>)
 RegisterCommand("changeradio", function(source, args)
+    local player = source
     if source then
         if #args == 1 then
             local newStation = args[1]
             if isValidStation(newStation) then
                 -- Triggers the client event to change the radio station for all emitters
-                TriggerClientEvent("changeRadioStationClient", -1, newStation)
+                TriggerClientEvent("changeRadioStationClient", -1,
+                    newStation)
                 print("Radio station changed to: " .. newStation)
             else
                 print("Invalid radio station: " .. newStation)
@@ -120,3 +136,5 @@ RegisterCommand("changeradio", function(source, args)
         print("Invalid player ID: " .. tostring(source))
     end
 end, true)
+=======
+>>>>>>> 72e210b047d2e282a7a0c0cab7ea24ad4e509820
