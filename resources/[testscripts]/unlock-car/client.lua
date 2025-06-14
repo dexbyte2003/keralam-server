3a0bda566de88af945b207d5fe21193836dbdf50
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-06-14 22:14:16 +0530

    added unlock car command to unlock nearest vehicle

---
+++ resources/[testscripts]/unlock-car/client.lua
@@ -0,0 +1,35 @@
+-- Ensure QBCore is loaded
+local QBCore = exports['qb-core']:Get                   -- Modern way to get QBCore export
+if not QBCore then
+    print('^1 [my_vehicle_unlock] QBCore not found, script might not function correctly! ^7')
+    return
+end
+
+-- CONFIGURATION
+local UNLOCK_KEY = 73 -- Default: 'K' key (check FiveM key mapping for others)
+local UNLOCK_RANGE = 5.0 -- Meters: How close you need to be to a vehicle
+local NOTIFICATION_DURATION = 2500 -- Milliseconds: How long notifications display
+
+
+-- You can add a command as an alternative or additional trigger
+RegisterCommand('unlockcar', function(source, args, rawCommand)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+
+    local closestVehicle = QBCore.Functions.GetClosestVehicle(playerCoords, UNLOCK_RANGE)
+
+    if closestVehicle and DoesEntityExist(closestVehicle) and GetEntityType(closestVehicle) == 2 then
+        local vehicleCoords = GetEntityCoords(closestVehicle)
+        local distance = #(playerCoords - vehicleCoords)
+
+        if distance <= UNLOCK_RANGE then
+            SetVehicleDoorsLocked(closestVehicle, 0)
+            SetVehicleUndriveable(closestVehicle, false)
+            QBCore.Functions.Notify('Vehicle unlocked via command!', 'success', NOTIFICATION_DURATION)
+        else
+            QBCore.Functions.Notify('No vehicle in range for command.', 'error', NOTIFICATION_DURATION)
+        end
+    else
+        QBCore.Functions.Notify('No vehicle found nearby for command.', 'error', NOTIFICATION_DURATION)
+    end
+end, false) -- The 'false' means it's a client-side command accessible by everyone
---
+++ resources/[testscripts]/unlock-car/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Make sure this script runs after QBCore is initialized
+dependencies {
+    'qb-core'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+-- Optional: You can specify an icon or description if you want
+-- author 'Your Name'
+-- description 'Unlock nearest vehicle script for QBCore'
