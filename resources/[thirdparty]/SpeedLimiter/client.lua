9ab9e76a6d87d7c5b8842f487403a51749768370
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-06-17 22:18:11 +0530

    Added custom cruise control script in SpeedLimiter Script

--- resources/[thirdparty]/SpeedLimiter/client.lua
+++ resources/[thirdparty]/SpeedLimiter/client.lua
@@ -92,3 +92,55 @@ RegisterCommand("setSpeed", function(source, args, rawCommand)
         })
     end
 end, false)
+
+
+
+local cruiseEnabled = false
+local cruiseSpeed = 0.0
+
+RegisterCommand("setcruise", function(source, args)
+    local playerPed = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    if vehicle and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, -1) == playerPed then
+        if #args < 1 then
+            QBCore.Functions.Notify("Usage: /setcruise [speed in km/h]", "error")
+            return
+        end
+
+        cruiseSpeed = tonumber(args[1])
+
+        if not cruiseSpeed or cruiseSpeed <= 0 then
+            QBCore.Functions.Notify("Please enter a valid speed (km/h) greater than 0", "error")
+            return
+        end
+
+        cruiseSpeed = cruiseSpeed / 3.6 -- Convert km/h to m/s
+        cruiseEnabled = true
+
+        QBCore.Functions.Notify("Cruise control activated at " .. args[1] .. " km/h", "success")
+    else
+        QBCore.Functions.Notify("You must be driving a vehicle to use this command", "error")
+    end
+end)
+
+-- Cancel cruise when brake is pressed
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+
+        if cruiseEnabled then
+            local playerPed = PlayerPedId()
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+            if not IsPedInAnyVehicle(playerPed, false) or IsControlPressed(0, 72) then -- Brake (default key is S)
+                cruiseEnabled = false
+                QBCore.Functions.Notify("Cruise control deactivated", "primary")
+            else
+                -- Maintain speed
+                SetVehicleForwardSpeed(vehicle, cruiseSpeed)
+            end
+        end
+    end
+end)
+
