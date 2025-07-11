68eee3e09fcdfc83425fa5832fd2b234b2e98bb2
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-07-12 04:36:08 +0530

    Added Gunfight location in muthukk

--- resources/[thirdparty]/GangShootout/config.lua
+++ resources/[thirdparty]/GangShootout/config.lua
@@ -59,4 +59,44 @@ Config.Locations = { -- To add more locations copy line 22-58 and paste it below
             vector3(1974.9384, 3052.2219, 47.1787)
         },
     },
+    ['Muthukk'] = { -- Can be named anything
+
+        start = vector3(-1968.7203, -532.5527, 12.1707, 313.4397), -- Will display the marker to start the shootout
+
+        count = 10, -- Amount of peds that will spawn
+
+        startText = 'Press ~INPUT_CONTEXT~ to start a fight', -- Text that will appear when in the start marker (~INPUT_CONTEXT~ makes the E showup)
+
+        marker = {
+            type = 1,
+            size = 1.7,
+            r = 255, 
+            g = 255,
+            b = 255,
+            a = 255
+        },
+
+        peds = { -- Peds that will spawn in when attack starts
+            -- GetHashKey('g_m_y_lost_01'),
+            -- GetHashKey('g_m_y_lost_02'),
+            -- GetHashKey('g_m_y_lost_03'),
+            GetHashKey('s_f_y_hooker_01'),
+            GetHashKey('s_f_y_hooker_02'),
+            GetHashKey('s_f_y_hooker_03'),
+        },
+
+        weapons = { -- Weapons that the peds will use for attack
+            'weapon_revolver',
+            'weapon_sawnoffshotgun',
+        },
+
+        spawnlocs = { -- Where peds will spawn when attack starts
+            vector3(-2059.147216796875, -475.6219787597656, 11.6717529296875)
+            -- vector3(1983.0621, 3026.5354, 47.7965),
+            -- vector3(1997.4485, 3039.3652, 47.0267),
+            -- vector3(1994.8364, 3046.3088, 47.2152),
+            -- vector3(1972.1107, 3056.3047, 46.9456),
+            -- vector3(1974.9384, 3052.2219, 47.1787)
+        },
+    },
 }
