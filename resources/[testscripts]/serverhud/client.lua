af097a24f62c032ebd3fdf3f52540a5a24134704
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-07-28 03:00:14 +0530

    Updated Hud

--- resources/[testscripts]/serverhud/client.lua
+++ resources/[testscripts]/serverhud/client.lua
@@ -10,10 +10,10 @@ local playerData = {}
 local config = {}
 config.ServerName = "Keralam" -- Change this to your server's name
 
-
-function getTime()
-    hour = GetClockHours()
-    minute = GetClockMinutes()
+-- Function to get current time in HH:MM format
+local function getTime()
+    local hour = GetClockHours()
+    local minute = GetClockMinutes()
     if hour <= 9 then
         hour = "0" .. hour
     end
@@ -22,7 +22,6 @@ function getTime()
     end
     return hour .. ":" .. minute
 end
-
 
 -- Function to update the NUI display
 local function UpdateUI()
@@ -47,12 +46,18 @@ local function UpdateUI()
     displayData.jobLabel = (playerData.job and playerData.job.label) or "None"
     displayData.jobGrade = (playerData.job and playerData.job.grade.name) or "None"
 
-    -- Server name
+    -- Server name & time
     displayData.serverName = config.ServerName
-
     displayData.serverTime = getTime()
 
-    displayData.totalPlayers = GetActivePlayers()
+    -- Total players (as count)
+    displayData.totalPlayers = #GetActivePlayers()
+
+    -- Send the data to NUI
+    SendNUIMessage({
+        type = "update",
+        data = displayData
+    })
 end
 
 -- On player loaded
@@ -75,10 +80,10 @@ RegisterNetEvent('QBCore:Player:OnJobUpdate', function(job)
     UpdateUI()
 end)
 
--- Periodic UI refresh
+-- Periodic UI refresh (every 10 seconds)
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(10000) -- every 10 seconds
+        Citizen.Wait(10000)
         local newData = QBCore.Functions.GetPlayerData()
         if newData then
             playerData = newData
@@ -87,7 +92,7 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Command to manually check data
+-- Command to manually refresh UI
 RegisterCommand("refreshui", function()
     local newData = QBCore.Functions.GetPlayerData()
     if newData then
