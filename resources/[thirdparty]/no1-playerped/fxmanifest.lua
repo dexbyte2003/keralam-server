1cc7063a42595bff9d564f84fd20cd3de0538fd8
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2025-09-24 19:19:27 +0530

    Added New Pedmenu Script

---
+++ resources/[thirdparty]/no1-playerped/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PlayerNo1'
+description 'A player ped script'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config/config.lua',
+    'config/peds.lua'
+}
+
+client_script 'client/main.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua', 
+    'server/main.lua'
+}
+
+files {
+    'locales/en.json'
+}
+
+lua54 'yes'
+
+dependency 'ox_lib'
