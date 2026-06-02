e1b8c3269bc1996b24a84098519d7c10dd8c2035
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2026-06-02 12:41:42 +0530

    Added TVA GH

---
+++ resources/[ymaps]/tva-gh/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+lua54 'yes'
+game "gta5"
+
+author 'TVA - Thakkudu Vava Adholokam'
+description 'TVA Gang House'
+version '1.0.0'
+
+this_is_a_map "yes"
+
+
+escrow_ignore {
+    'stream/*.ytd'
+}
+dependency '/assetpacks'
+
+server_scripts {
+	--[[server.lua]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            'html/.rollup.config.js',
+}
